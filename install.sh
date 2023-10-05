6c344f2d83e641838a65423b9970344f661fe600
phiyerion <reidbandy@gmail.com>
2023-10-04 21:40:57 -0400

    bug fixes

--- install.sh
+++ install.sh
@@ -5,11 +5,11 @@ git submodules init
 git submoduels update
 
 cd $WD/dmenu
-make clean install
+sudo make clean install
 cd $WD/st
-make clean install
+sudo make clean install
 cd $WD
-make clean install
+sudo make clean install
 
 wget 'https://r4.wallpaperflare.com/wallpaper/906/970/555/digital-art-eclipse-clouds-berserk-wallpaper-c970584d01facd0b06a7688f9071767d.jpg' -O ~/Pictures/wallpaper.jpg;
 
